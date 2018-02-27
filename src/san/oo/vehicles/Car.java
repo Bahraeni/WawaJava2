cd9b44db1440c567a56752b4f6bab60c0f68f770
kongra <kongra@gmail.com>
2018-02-27 15:25:50 +0100

    -

---
+++ src/san/oo/vehicles/Car.java
@@ -0,0 +1,15 @@
+package san.oo.vehicles;
+
+public class Car {
+
+  private boolean running;
+
+  public void run() {
+    running = true;
+  }
+
+  public void stop() {
+    running = false;
+  }
+
+}
---
+++ src/san/oo/vehicles/CarTest.java
@@ -0,0 +1,12 @@
+package san.oo.vehicles;
+
+public class CarTest {
+
+  public static void main(String[] args) {
+    Car c1 = new Car();
+    c1.run();
+
+    System.out.println("Finished.");
+  }
+
+}
