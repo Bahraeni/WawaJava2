e7b79d8e9e70a972ae8f8df10ca270f2dacbc287
kongra <kongra@gmail.com>
2018-03-13 12:51:59 +0100

    -

---
+++ www-eng/hello.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Hello PHP Page
+    </title>
+  </head>
+  <body>
+    <h1>Hello Dear User <?php
+        $name = "John";
+        echo($name);
+      ?> !!!</h1>
+  </body>
+</html>
+
+<!-- Put me into: c:\xampp\htdocs\ -->
